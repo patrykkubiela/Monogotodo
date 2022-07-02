76e813d59518f9908ed851546212449499e64105
KUBIELA Patryk <patryk.kubiela-ext@leica-geosystems.com>
2022-07-02 20:00:16 +0200

    Rename crumbs to monogoto in Api project

---
+++ Monogotodo.Api/Controllers/MonogotoController.cs
@@ -0,0 +1,24 @@
+﻿using Monogotodo.Data.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Monogotodo.Api.Controllers
+{
+    [ApiController]
+    [Route("crumb")]
+    public class MonogotoController: ControllerBase
+    {
+        private readonly IMonogotoRepository _monogotoRepository;
+
+        public MonogotoController(IMonogotoRepository monogotoRepository)
+        {
+            _monogotoRepository = monogotoRepository;
+        }
+        
+        [HttpGet]
+        public IActionResult GetTest()
+        {
+            var monogotos = _monogotoRepository.GetMonogotos("SELECT * FROM public.\"Queries\"");
+            return Ok("Testing crumb controller");
+        }
+    }
+}
