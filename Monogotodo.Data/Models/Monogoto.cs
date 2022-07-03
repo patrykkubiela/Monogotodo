af957f0f383ee8cbcfb302860c838d5a10623c87
KUBIELA Patryk <patryk.kubiela-ext@leica-geosystems.com>
2022-07-03 09:49:10 +0200

    Make monogoto model as bson mongodb type

--- Monogotodo.Data/Models/Monogoto.cs
+++ Monogotodo.Data/Models/Monogoto.cs
@@ -1,14 +1,28 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 using Monogotodo.Shared;
 
 namespace Monogotodo.Data.Models
 {
     public class Monogoto
     {
-        public Guid Uuid { get; }
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; }
+        
+        [BsonElement("name")]
         public string Name { get; set; }
+        
+        [BsonElement("description")]
         public string Description { get; set; }
+        
+        [BsonElement("type")]
         public MonogotoType Type { get; set; }
+        
+        [BsonElement("broadcaster")]
         public Monogoto Broadcaster { get; set; }
+        
+        [BsonElement("observers")]
         public ICollection<Monogoto> Observers { get; }
     }
 }
